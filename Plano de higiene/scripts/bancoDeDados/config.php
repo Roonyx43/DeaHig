55f54128f6ca342f4830d472f721df0804cd8442
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2024-10-21 16:51:45 -0300

    Add files via upload

---
+++ Plano de higiene/scripts/bancoDeDados/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $MYSQLDATABASE = 'railway';
+    $MYSQLHOST = 'viaduct.proxy.rlwy.net';
+    $MYSQLPASSWORD = 'fciTFQVmDVPzwUXFlZcTZenmCmAnpSTl';
+    $MYSQLUSER = 'root';
+    $MYSQLPORT = '45439';
+
+    $conn = new mysqli($MYSQLHOST, $MYSQLUSER, $MYSQLPASSWORD, $MYSQLDATABASE, $MYSQLPORT);
+
+?>
